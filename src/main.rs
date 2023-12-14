7d8e252a0bc77da220fa68aab6bb8e549a9eec61
k-srivastava <srivastavak1223@gmail.com>
2023-12-14 14:37:00 +0530

    Added a fuzziness property to metals.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
     let ground_material = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let center_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
 
-    let left_material = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let right_material = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let left_material = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let right_material = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.objects.push(Arc::new(Sphere::new(Point3D::new(0.0, -100.5, -1.0), 100.0, ground_material)));
     world.objects.push(Arc::new(Sphere::new(Point3D::new(0.0, 0.0, -1.0), 0.5, center_material)));
--- src/material.rs
+++ src/material.rs
@@ -35,23 +35,22 @@ impl Material for Lambertian {
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
-    }
+    pub fn new(albedo: Color, fuzz: f64) -> Self { Self { albedo, fuzz } }
 }
 
 impl Material for Metal {
     fn scatter(&self, ray_in: &Ray, record_point: &Vector3D, record_normal: &Vector3D, attenuation: &mut Color) -> Option<Ray> {
         let reflected = Vector3D::reflect(&ray_in.direction().normalized(), record_normal);
-        let scattered = Ray::new(record_point.clone(), reflected);
+        let scattered = Ray::new(record_point.clone(), reflected + Vector3D::random_normal() * self.fuzz);
 
         attenuation[0] = self.albedo.x();
         attenuation[1] = self.albedo.y();
         attenuation[2] = self.albedo.z();
 
-        Some(scattered)
+        if Vector3D::dot(&scattered.direction(), record_normal) > 0.0 { Some(scattered) } else { None }
     }
 }
